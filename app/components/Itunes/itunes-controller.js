a23166f0aeea7da453693f87d2508890fc35c7a5
Michael Green <michael.green94@hotmail.com>
2018-08-10 14:55:41 -0600

    functional

--- app/components/Itunes/itunes-controller.js
+++ app/components/Itunes/itunes-controller.js
@@ -7,9 +7,27 @@ const itunesService = new ItunesService()
 function drawSongs(results) {
   console.log(results)
   //YOUR CODING STARTS HERE
+  let template = ''
+  for (let i = 0; i < results.length; i++) {
+    const song = results[i];
+    template += `
+  <div class="col-sm-3 card bg-secondary mx-1 my-1">
+      <img class="card-img" src="${song.albumArt}" />
+    <div class="card-img-overlay d-flex justify-content-end">
+      <audio controls>
+        <source src="${song.preview}">
+      </audio>
+    </div>
+    <div class="card-body">
+      <h5 class="card-title">${song.title} $${song.price}</h5>
+      <h5 class="card-text">By: ${song.artist}</h5>
+      <h5 class="card-text"><i class="fas fa-compact-disc"></i> ${song.collection}</h5>
+    </div>
+  </div>`
 
+  }
 
-
+  document.getElementById('songs').innerHTML = template
 }
 
 
--- app/components/Itunes/itunes-service.js
+++ app/components/Itunes/itunes-service.js
@@ -1,21 +1,30 @@
 import Song from "../../models/Song.js";
 
-
+//DO NOT MODIFY
 class ItunesService {
 
-  //DO NOT MODIFY
+  //   getMusicByArtist(artist) {
+  //     var url = 'https://itunes.apple.com/search?callback=?&term=' + artist;
+  //     // @ts-ignore
+  //     return $.getJSON(url)
+  //       .then(res => res.results.map(s => new Song(s)))
+  //       .catch(err => console.log(err))
+  //   }
+
   getMusicByArtist(artist) {
-    var url = 'https://itunes.apple.com/search?term=' + artist;
+
+    var url1 = '//bcw-getter.herokuapp.com/?url=';
+    var url2 = 'https://itunes.apple.com/search?term=' + artist
+    var apiUrl = url1 + encodeURIComponent(url2);
     //Casts each object to 
-    return $.getJSON(url).then(function (response) {
-      var songList = response.results.map(s => { return new Song(s) })
+    return $.getJSON(apiUrl).then(function (response) {
+      var songList = response.results.map(s => {
+        return new Song(s)
+      })
       return songList;
     })
   }
-
-
 }
-
 
 
 export default ItunesService
