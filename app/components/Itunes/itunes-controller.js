63c4e2a2d1257751fd095978ed5ad53940c2df94
Michael Green <michael.green94@hotmail.com>
2018-08-10 16:50:15 -0600

    finished checkpoint

--- app/components/Itunes/itunes-controller.js
+++ app/components/Itunes/itunes-controller.js
@@ -11,14 +11,14 @@ function drawSongs(results) {
   for (let i = 0; i < results.length; i++) {
     const song = results[i];
     template += `
-  <div class="col-sm-6 col-md-4 col-lg-2 mx-1 my-1 card bg-opac">
-      <img class="card-img" src="${song.albumArt}" />
-    <div class="card-img-overlay aud-button">
+  <div class="col-sm-6 col-md-4 col-lg-2 mx-2 my-2 card bg-opac">
+      <img class="card-img-top" src="${song.albumArt}" />
+    <div class="card-img-overlay-bottom d-flex aud-button">
       <audio controls>
         <source src="${song.preview}">
       </audio>
     </div>
-    <div class="card-body">
+    <div class="card-body pt0 mt-20 ">
       <h6 class="card-title text-truncate">${song.title} $${song.price}</h6>
       <h6 class="card-text text-truncate">By: ${song.artist}</h6>
       <h6 class="card-text text-truncate"><i class="fas fa-compact-disc"></i> ${song.collection}</h6>
